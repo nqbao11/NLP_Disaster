568a3db35b5688352a4ab72b6b65bae3711e907e
BaoNQ24 <nqbao11.01@gmail.com>
2022-06-20 14:28:32 +0700

    Add eval.py

--- src/eval.py
+++ src/eval.py
@@ -1,7 +1,20 @@
+import argparse
+import subprocess
+import os
+
+
 if __name__ == "__main__":
-    ...
-    #Load evaluate data
+    #Define argument
+    parser = argparse.ArgumentParser(description="Model Evalution")
+    parser.add_argument("--model", required=True, help="model directory")
+    parser.add_argument("--data", required=True, help="evaluation data directory")
+    parser.add_argument("-o", "--output",required=True, help="Output directory")
 
-    #Load model
+    args = parser.parse_args()
+    
+    
 
-    #Evaluate model
+    #Call for evaluation
+    subprocess.run(["python", os.path.join(args.model, "infer.py"), 
+                    "--data", args.data, 
+                    "--output", args.output])
